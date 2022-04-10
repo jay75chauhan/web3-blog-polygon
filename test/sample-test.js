83034fe5039a4c8bf858867d0e9db6ef6db31123
jay75chauhan <chauhan75jayk@gmail.com>
2022-04-10 14:58:42 +0530

    initial commit

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,36 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Blog", async function () {
+  it("Should create a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("My blog");
+    await blog.deployed();
+    await blog.createPost("My first post", "12345");
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const posts = await blog.fetchPosts();
+    expect(posts[0].title).to.equal("My first post");
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should edit a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("My blog");
+    await blog.deployed();
+    await blog.createPost("My Second post", "12345");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await blog.updatePost(1, "My updated post", "23456", true);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    posts = await blog.fetchPosts();
+    expect(posts[0].title).to.equal("My updated post");
+  });
+
+  it("Should add update the name", async function () {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("My blog");
+    await blog.deployed();
+
+    expect(await blog.name()).to.equal("My blog");
+    await blog.updateName("My new blog");
+    expect(await blog.name()).to.equal("My new blog");
   });
 });
